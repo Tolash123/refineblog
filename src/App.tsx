3936655d4ec00e543fcdca6b8aba40cacb65a6f4
Tolash123 <91490492+Tolash123@users.noreply.github.com>
2023-04-24 23:25:24 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,48 @@
-import {
-    GitHubBanner,
-    Refine,
-    WelcomePage,
-    ErrorComponent,
-} from "@refinedev/core";
-import dataProvider from "@refinedev/simple-rest";
-import routerProvider, {
+import { Refine } from "@refinedev/core";
+import routerBindings, {
     UnsavedChangesNotifier,
 } from "@refinedev/react-router-v6";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import dataProvider from "@refinedev/simple-rest";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HeadlessInferencer } from "@refinedev/inferencer/headless";
 
-function App() {
+const App = () => {
     return (
         <BrowserRouter>
-            <GitHubBanner />
             <Refine
-                routerProvider={routerProvider}
+                routerProvider={routerBindings}
                 dataProvider={dataProvider("https://api.fake-rest.refine.dev")}
+                resources={[
+                    {
+                        name: "blog_posts",
+                        list: "/blog-posts",
+                        show: "/blog-posts/show/:id",
+                        create: "/blog-posts/create",
+                        edit: "/blog-posts/edit/:id",
+                    },
+                ]}
                 options={{
                     syncWithLocation: true,
                     warnWhenUnsavedChanges: true,
                 }}
             >
                 <Routes>
-                    <Route index element={<WelcomePage />} />
-
-                    <Route path="*" element={<ErrorComponent />} />
+                    <Route path="blog-posts">
+                        <Route index element={<HeadlessInferencer />} />
+                        <Route
+                            path="show/:id"
+                            element={<HeadlessInferencer />}
+                        />
+                        <Route
+                            path="edit/:id"
+                            element={<HeadlessInferencer />}
+                        />
+                        <Route path="create" element={<HeadlessInferencer />} />
+                    </Route>
                 </Routes>
                 <UnsavedChangesNotifier />
             </Refine>
         </BrowserRouter>
     );
-}
-
+};
 export default App;
